6c76cbdc94be1f07327f64de4974264314f4f225
jrandiny <jrandiny2@gmail.com>
2019-04-18 20:49:14 +0700

    Implement TimerObj
    Co-Authored-By: SteveImmanuel <steveandreasimanuel@gmail.com>
    Co-Authored-By: a-johanes <antoniusjohanes99@gmail.com>
    Co-Authored-By: willysantoso05 <willysantoso05@gmail.com>

---
+++ src/main/java/id/backendk3/engifarm/TimerObj.java
@@ -0,0 +1,74 @@
+package id.backendk3.engifarm;
+
+/**
+ * Kelas abstrak TimerObj
+ * 
+ * <p> Kelas ini merepresentasikan timer untuk setiap objek turunannya
+ * Timer sebuah objek akan diinisiasi dengan nilai dan akan berkurang setiap ticknya.
+ * Jika timer objek sudah habis maka akan memanggil method callback yang dioverride oleh kelas turunannya 
+ * 
+ * @author backendk3
+ * @see id.backendk3.engifarm.Cell.Cell
+ */
+public abstract class TimerObj{
+    private int timer;
+    private boolean timerActive;
+
+    /**
+     * Konstruktor Kelas TimerObj
+     */
+    public TimerObj(){
+        timerActive = false;
+        timer = 0;
+    }
+
+    /**
+     * Method abstrak callback
+     */
+    public abstract void callback();
+
+    /**
+     * Prosedur yang dilakukan setiap satu satuan tick pada timer
+     */
+    public void tick(){
+        if(timerActive){
+            timer--;
+            if(timer==0){
+                timerActive = false;
+                callback();
+            }
+        }
+    }
+
+    /**
+     * Fungsi yang mengembalikan kondisi timerActive
+     * @return True jika timer aktif
+     */
+    public boolean getTimerActive(){
+        return timerActive;
+    }
+
+    /**
+     * Fungsi yang mengembalikan waktu sisa timer
+     * @return waktu sisa timer
+     */
+    public int getTimer(){
+        return timer;
+    }
+
+    /**
+     * Method yang menetapkan kondisi timerActive
+     * @param _timerActive kondisi timer
+     */
+    public void setTimerActive(boolean _timerActive){
+        timerActive = _timerActive;
+    }
+
+    /**
+     * Method yang menetapkan waktu timer
+     * @param _timer waktu timer
+     */
+    public void setTimer(int _timer){
+        timer = _timer;
+    }
+}
